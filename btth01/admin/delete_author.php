61d5d086acb53e61c78860f0b636b4ecac2aa205
NguyenPhuongTrinh <2051063817.e.tlu.edu.vn>
2023-02-27 04:08:30 +0700

    them chuc nang sua xoa tai khoan

--- btth01/admin/delete_author.php
+++ btth01/admin/delete_author.php
@@ -8,10 +8,10 @@ if(!$id){
     redirect('author.php', ['failure' => 'Không tìm thấy tác giả']);
 }
 
-$article = false;
+$result = false;
 $sql = "SELECT ten_tgia, hinh_tgia FROM tacgia WHERE ma_tgia = :ma_tgia";
-$article = pdo($pdo, $sql, ['ma_tgia' => $id])->fetch();
-if(!$article){
+$result = pdo($pdo, $sql, ['ma_tgia' => $id])->fetch();
+if(!$result){
     redirect('author.php', ['failure' => 'Không tìm thấy tác giả']);
 }
 
@@ -20,6 +20,7 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
     pdo($pdo, $sql, ['ma_tgia' => $id]);
     $sql = "DELETE FROM tacgia WHERE ma_tgia = :ma_tgia";
     pdo($pdo, $sql, ['ma_tgia' => $id]);
+    unlink('../images/authors/'.$result['hinh_tgia']);
     redirect('author.php', ['success' => 'Đã xóa tác giả']);
 }
 ?>
@@ -27,7 +28,7 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
     <main class="container admin d-flex justify-content-center" id="content" style="min-height: 50vh; margin-top: 200px">
         <form action="delete_author.php?id=<?= $id; ?>" method="POST" class="narrow">
             <h1 class="m-3">Xóa tác giả</h1>
-            <p>Bạn có chắc chắn muốn xóa tác giả: <em><?= html_escape($article['ten_tgia']) ?></em>?</p>
+            <p>Bạn có chắc chắn muốn xóa tác giả: <em><?= html_escape($result['ten_tgia']) ?></em>?</p>
             <div class="row" margin>
                 <div class="col-6">
                     <input type="submit" name="delete" value="Xóa" class="btn btn-danger">
